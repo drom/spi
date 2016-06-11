f03ff3741f950f2b068944b3586ccaa4d15b3631
Aliaksei Chapyzhenka <alex.drom@gmail.com>
2016-06-11 07:59:25 +0300

    basic instruction command

--- img/inst.wavedrom.js
+++ img/inst.wavedrom.js
@@ -1,1 +1,11 @@
-{}
+{signal: [
+  {name: 'cmd_dat', wave: 'x3x', data: 'C0' },
+  {name: 'cmd_req', wave: '010' },
+  {name: 'cmd_res', wave: '1.0' },
+  {},
+  {name: 'SS#', wave: '=.0.........'},
+  {name: 'SCK', wave: '=..n........'},
+  {name: 'IO0', wave: 'x..44444444x', data: '7 6 5 4 3 2 1 0'},
+],
+  head: {tock: -1}
+}
