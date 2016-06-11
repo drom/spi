a5e1da24a5bc607ebc0ea78f6026ab8dc9441cbb
Aliaksei Chapyzhenka <alex.drom@gmail.com>
2016-06-11 08:34:19 +0300

    added coo

---
+++ img/coo.wavedrom.js
@@ -0,0 +1,13 @@
+{signal: [
+  {name: 'cmd_dat', wave: 'x3', data: 'C2' },
+  {name: 'cmd_req', wave: '01' },
+  {name: 'cmd_res', wave: '1.0' },
+  {},
+  {name: 'SS#',   wave: '=.0.........................'},
+  {name: 'SCK',   wave: '=..n........................'},
+  {name: 'IO0',   wave: 'x..44444444x................', data: '7 6 5 4 3 2 1 0'},
+  {name: 'IO1',   wave: 'z..........5555555555555555x', data: '7 6 5 4 3 2 1 0 7 6 5 4 3 2 1 0'},
+  {name: 'phase', wave: '=..=.......=.......=.......=', data: ' Instruction D0 D1'}
+],
+ config: {skin: 'narrow'}, head: {tock: -1}
+}
