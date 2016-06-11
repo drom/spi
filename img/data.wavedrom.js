4e94715f5c106b3d8e3837454f39cc0030e48d20
Aliaksei Chapyzhenka <alex.drom@gmail.com>
2016-06-11 08:26:48 +0300

    switch to narrow skin

--- img/data.wavedrom.js
+++ img/data.wavedrom.js
@@ -8,5 +8,5 @@
   {name: 'IO0', wave: 'x..4444444455555555x', data: '7 6 5 4 3 2 1 0 7 6 5 4 3 2 1 0'},
   {name: 'IO1', wave: 'z...................'}
 ],
-  head: {tock: -1}
+ config: {skin: 'narrow'}, head: {tock: -1}
 }
