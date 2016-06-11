0c29dcc51d7a30014fb940b556b958a55ebd2124
Aliaksei Chapyzhenka <alex.drom@gmail.com>
2016-06-11 08:14:55 +0300

    added simple write

---
+++ img/data.wavedrom.js
@@ -0,0 +1,12 @@
+{signal: [
+  {name: 'cmd_dat', wave: 'x3x', data: 'C1' },
+  {name: 'cmd_req', wave: '010' },
+  {name: 'cmd_res', wave: '1.1' },
+  {},
+  {name: 'SS#', wave: '=.0.................'},
+  {name: 'SCK', wave: '=..n................'},
+  {name: 'IO0', wave: 'x..4444444455555555x', data: '7 6 5 4 3 2 1 0 7 6 5 4 3 2 1 0'},
+  {name: 'IO1', wave: 'z...................'}
+],
+  head: {tock: -1}
+}
