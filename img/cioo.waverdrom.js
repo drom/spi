289754216d18d5bd29a43bce9533cdef1a59025a
Aliaksei Chapyzhenka <alex.drom@gmail.com>
2016-06-11 08:51:59 +0300

    added cioo

---
+++ img/cioo.waverdrom.js
@@ -0,0 +1,13 @@
+{signal: [
+  {name: 'tdat', wave: 'x3', data: 'C' },
+  {name: 'treq', wave: '01' },
+  {name: 'tres', wave: '1.0' },
+  {},
+  {name: 'SS#',   wave: '=.0.................................'},
+  {name: 'SCK',   wave: '=..n................................'},
+  {name: 'IO0',   wave: 'x..4444444455555555x................', data: '7 6 5 4 3 2 1 0 7 6 5 4 3 2 1 0'},
+  {name: 'IO1',   wave: 'z..................5555555555555555x', data: '7 6 5 4 3 2 1 0 7 6 5 4 3 2 1 0'},
+  {name: 'phase', wave: '=..=.......=.......=.......=.......=', data: ' Instruction i0 o0 o1'}
+],
+ config: {skin: 'narrow'}, head: {tock: -1}
+}
