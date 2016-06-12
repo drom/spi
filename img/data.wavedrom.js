03e961749d19f4ec111af6d989fdff9ac03db1d1
Aliaksei Chapyzhenka <alex.drom@gmail.com>
2016-06-12 12:21:42 +0300

    added req/res interfaces

--- img/data.wavedrom.js
+++ img/data.wavedrom.js
@@ -1,12 +1,17 @@
 {signal: [
-  {name: 'tdat', wave: 'x3', data: 'C1' },
-  {name: 'treq', wave: '01' },
-  {name: 'tres', wave: '1.0' },
+  {name: 'cmdd',  wave: 'x3x', data: 'C' },
+  {name: 'cmdq',  wave: '010' },
+  {name: 'cmda',  wave: '1..' },
   {},
-  {name: 'SS#', wave: '=.0.................'},
-  {name: 'SCK', wave: '=..n................'},
-  {name: 'IO0', wave: 'x..4444444455555555x', data: '7 6 5 4 3 2 1 0 7 6 5 4 3 2 1 0'},
-  {name: 'IO1', wave: 'z...................'}
+  {name: 'reqd',  wave: '3.x', data: 'D0', phase: -8 },
+  {name: 'reqq',  wave: '1.0',             phase: -8 },
+  {name: 'reqa',  wave: '01.',             phase: -8 },
+  {},
+  {name: 'SS#',   wave: '=.0.................'},
+  {name: 'SCK',   wave: '=..n................'},
+  {name: 'IO0',   wave: 'x..4444444455555555x', data: '7 6 5 4 3 2 1 0 7 6 5 4 3 2 1 0'},
+  {name: 'IO1',   wave: 'z...................'},
+  {name: 'phase', wave: '=..=.......=.......=', data: ' instruction request'}
 ],
   head: {tock: -1}
 }
--- img/inst.wavedrom.js
+++ img/inst.wavedrom.js
@@ -6,7 +6,8 @@
   {name: 'SS#', wave: '=.0.........'},
   {name: 'SCK', wave: '=..n........'},
   {name: 'IO0', wave: 'x..44444444x', data: '7 6 5 4 3 2 1 0'},
-  {name: 'IO1', wave: 'z...........'}
+  {name: 'IO1', wave: 'z...........'},
+  {name: 'phase', wave: '=..=.......=', data: ' Instruction'}
 ],
   head: {tock: -1}
 }
